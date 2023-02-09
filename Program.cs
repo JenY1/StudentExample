960c10d8d0c4b8a3c016c70260e9da14f4bb8550
Jenny <jenny.reeve91@gmail.com>
2023-02-09 19:43:35 +1000

    Student coding example

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var students = new List<Student>() {
+      	new Student() { FirstName = "Xavier", LastName = "Thorpe" },
+        new Student() { FirstName = "Bianca", LastName = "Barclay" },
+        new Student() { FirstName = "Eugene", LastName = "Otinger" },
+        new Student() { FirstName = "Enid", LastName = "Sinclair" },
+        new Student() { FirstName = "Wednesday", LastName = "Addams" }
+      };
+      
+      Console.WriteLine("-- Student List --");
+      foreach (var student in students) {
+      	Console.WriteLine(student.Name);
+      }
+    }
+  }
+  
+   public class Student {
+      public string? FirstName { get; set; }  
+      public string? LastName { get; set; }
+      public string Name => $"{FirstName} {LastName}";
+  	}
+}
